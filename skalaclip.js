a99b1ccbffebfbbb5d545cce52378dfded8a0817
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-08-07 10:29:24 +0200

    Linting

--- skalaclip.js
+++ skalaclip.js
@@ -3,15 +3,15 @@ function Point(x, y) {
 }
 
 function Vector(x, y, z) {
-  return { x: x, y: y, z: z };
+  return { x, y, z };
 }
 
-function point_cross(a, b) {
+function pointCross(a, b) {
   return Vector(a.y - b.y, b.x - a.x, a.x * b.y - a.y * b.x);
 }
 
 function cross(u, v) {
-  var z = u.x * v.y - u.y * v.x;
+  const z = u.x * v.y - u.y * v.x;
   return Point((u.y * v.z - u.z * v.y) / z, (u.z * v.x - u.x * v.z) / z);
 }
 
@@ -19,9 +19,9 @@ function dot(u, v) {
   return u.x * v.x + u.y * v.y + u.z * v.z;
 }
 
-let mask = [0, 1, 2, 2, 4, 0, 4, 4, 8, 1, 0, 2, 8, 1, 8, 0];
-let tab1 = [4, 3, 0, 3, 1, 4, 0, 3, 2, 2, 4, 2, 1, 1, 0, 4];
-let tab2 = [4, 0, 1, 1, 2, 4, 2, 2, 3, 0, 4, 1, 3, 0, 3, 4];
+const mask = [0, 1, 2, 2, 4, 0, 4, 4, 8, 1, 0, 2, 8, 1, 8, 0];
+const tab1 = [4, 3, 0, 3, 1, 4, 0, 3, 2, 2, 4, 2, 1, 1, 0, 4];
+const tab2 = [4, 0, 1, 1, 2, 4, 2, 2, 3, 0, 4, 1, 3, 0, 3, 4];
 
 class Clipper {
   constructor(a, b) {
@@ -45,19 +45,19 @@ class Clipper {
       Vector(this.x_min, this.y_min, 1),
       Vector(this.x_max, this.y_min, 1),
       Vector(this.x_max, this.y_max, 1),
-      Vector(this.x_min, this.y_max, 1)
-    ]
+      Vector(this.x_min, this.y_max, 1),
+    ];
 
     this.e = [
       Vector(0, 1, -this.y_min),
       Vector(1, 0, -this.x_max),
       Vector(0, 1, -this.y_max),
-      Vector(1, 0, -this.x_min)
-    ]
+      Vector(1, 0, -this.x_min),
+    ];
   }
 
   code(pt) {
-    var c = 0;
+    let c = 0;
 
     if (pt.x < this.x_min) {
       c = 8;
@@ -74,55 +74,61 @@ class Clipper {
     return c;
   };
 
-  clipLine(x_a, x_b) {
-    let c_a = this.code(x_a);
-    let c_b = this.code(x_b);
+  clipLine(xA, xB) {
+    const cA = this.code(xA);
+    const cB = this.code(xB);
 
-    if ((c_a | c_b) == 0) {
-      return [0, x_a, x_b];
+    if ((cA | cB) == 0) {
+      return [0, xA, xB];
     }
 
-    if ((c_a & c_b) != 0) {
-      return [-1, x_a, x_b];
+    if ((cA & cB) != 0) {
+      return [-1, xA, xB];
     }
 
-    let p = point_cross(x_a, x_b);
+    const p = pointCross(xA, xB);
 
-    var c = 0;
+    let c = 0;
 
-    for (var k = 0; k < 4; k++) {
+    for (let k = 0; k < 4; k += 1) {
       if (dot(p, this.x[k]) <= 0) {
         c |= (1 << k);
       }
     }
 
-    if (c == 0 || c == 15) {
-      return [-1, x_a, x_b];
+    if (c === 0 || c === 15) {
+      return [-1, xA, xB];
     }
 
-    let i = tab1[c];
-    let j = tab2[c];
+    const i = tab1[c];
+    const j = tab2[c];
 
-    if (c_a != 0 && c_b != 0) {
-      x_a = cross(p, this.e[i]);
-      x_b = cross(p, this.e[j]);
-      return [3, x_a, x_b];
-    } else {
-      if (c_a == 0) {
-        if ((c_b & mask[c]) == 0) {
-          x_b = cross(p, this.e[i]);
-        } else {
-          x_b = cross(p, this.e[j]);
-        }
-        return [2, x_a, x_b];
-      } else if (c_b == 0) {
-        if ((c_a & mask[c]) == 0) {
-          x_a = cross(p, this.e[i]);
-        } else {
-          x_a = cross(p, this.e[j]);
-        }
-        return [1, x_a, x_b];
+    if (cA !== 0 && cB !== 0) {
+      const newXA = cross(p, this.e[i]);
+      const newXB = cross(p, this.e[j]);
+      return [3, newXA, newXB];
+    }
+
+    if (cA === 0) {
+      let newXB = null;
+
+      if ((cB & mask[c]) == 0) {
+        newXB = cross(p, this.e[i]);
+      } else {
+        newXB = cross(p, this.e[j]);
       }
+      return [2, xA, newXB];
+    }
+
+    if (cB === 0) {
+      let newXA = null;
+
+      if ((cA & mask[c]) == 0) {
+        newXA = cross(p, this.e[i]);
+      } else {
+        newXA = cross(p, this.e[j]);
+      }
+      return [1, newXA, xB];
     }
   }
 }
